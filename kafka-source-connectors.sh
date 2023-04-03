f24b20143a15ada26fa79f07a99c82c0c6922682
sabari <sabarikrishnavk@gmail.com>
2023-04-03 14:43:57 +1000

    Update attrdesc

--- kafka-source-connectors.sh
+++ kafka-source-connectors.sh
@@ -50,19 +50,21 @@ db2_catentry_attribute()
       "connection.password": "password",
       "topic.prefix": "db2.product.attributes",
       "mode":"timestamp",
-      "query":"SELECT CATENTRY_ID, PARTNUMBER, LASTUPDATE ,CAST(attr_id as varchar) attr_id, attr_value \
+      "query":"SELECT CATENTRY_ID, PARTNUMBER, LASTUPDATE ,attr_name,  attr_value \
               FROM ((select ca.CATENTRY_ID , ce.PARTNUMBER, ce.LASTUPDATE , \
-              avd.attr_id as attr_id, avd.stringvalue as attr_value \
-              from DB2ADMIN.catentryattr ca ,DB2ADMIN.attrvaldesc avd, DB2ADMIN.catentry CE \
+              ad.NAME as attr_name,  avd.stringvalue as attr_value \
+              from DB2ADMIN.catentryattr ca ,DB2ADMIN.attrvaldesc avd,DB2ADMIN.attrdesc ad, DB2ADMIN.catentry CE \
               where ca.catentry_id = CE.catentry_id \
+              and ad.ATTR_ID = ca.ATTR_ID \
               and avd.attrval_id = ca.attrval_id ) \
               UNION \
               (select rel.catentry_id_parent CATENTRY_ID, c.PARTNUMBER, c.LASTUPDATE , \
-              avd.attr_id as attr_id, avd.stringvalue as attr_value \
-              from DB2ADMIN.catentryattr ca, DB2ADMIN.attrvaldesc avd, DB2ADMIN.catentry c, DB2ADMIN.CATENTREL rel \
+              ad.NAME as attr_name,  avd.stringvalue as attr_value \
+              from DB2ADMIN.catentryattr ca, DB2ADMIN.attrvaldesc avd, DB2ADMIN.attrdesc ad, DB2ADMIN.catentry c, DB2ADMIN.CATENTREL rel \
               where c.catentry_id=rel.catentry_id_parent \
               and c.catenttype_id='ProductBean' and rel.catreltype_id='PRODUCT_ITEM' \
               and rel.catentry_id_child = ca.catentry_id \
+              and ad.ATTR_ID = ca.ATTR_ID \
               and avd.attrval_id = ca.attrval_id ))  ",
 
       "timestamp.column.name": "LASTUPDATE",
